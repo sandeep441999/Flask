456fd2cf50a286b6116f82b9bd9c258ad12932cb
sandeep441999 <64533970+sandeep441999@users.noreply.github.com>
2021-04-22 21:44:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///todos.sqlite3'
-app.config['SECRET_KEY']="sandeep pabbu"
 
 db=SQLAlchemy(app)
 
